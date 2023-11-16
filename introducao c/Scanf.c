4cb1f1cf03abb79da5285d0bf001e03fbd0b8dd8
reinoldes <reinoldesxbox@gmail.com>
2023-11-16 16:31:58 -0300

    Scanf

---
+++ introducao c/Scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+	int age = 0;
+	
+	printf("Valor inicial da idade: %d.\n", age);
+
+	printf("Digite uma idade:\n");
+	scanf("%d", &age);
+
+	printf("Idade Informada: %d. \n", age);
+
+}
--- introducao c/introducaoc.c
+++ introducao c/introducaoc.c
@@ -11,10 +11,21 @@ int main(int argc, char *argv[]) {
 	
 	Sleep(1500);
 
-	printf("\aValor inteiro: %d. \n", 10);
+	printf("\a\nValor inteiro: %d. \n", 10);
 	printf("Valor real: %f. \n", 3.14158265);
 	printf("Valor real com 2 casas: %.2f \n", 3.14158265);
 	printf("Dado de texto: %c. \n", 'a');
 	printf("Dado de texto: %s. \n", "testando");
+
+	Sleep(1500);
+
+	int age = 0;
+
+	printf("\a\nValor inicial da idade: %d.\n", age);
+
+	printf("Digite uma idade:\n");
+	scanf_s("%d", &age);
+
+	printf("Idade Informada: %d. \n", age);
 
 }
