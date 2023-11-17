08a07638dd837cfc8e5ee327da566f61a950276c
reinoldes <reinoldesxbox@gmail.com>
2023-11-17 09:57:06 -0300

    If

---
+++ Introducao C 3/Introducao C 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#pragma warning(disable : 4996).
+
+int main() {
+
+	float m;
+
+	printf("Insira a nota: \n");
+	scanf("%f", &m);
+
+	if (m >= 6.0) {
+		printf("Aprovado!\n");
+	}
+	else {
+		printf("Reprovado!\n");
+	}
+
+	if (m >= 4.0 && m < 6.0) {
+		printf("Tem direito a exame!\n");
+	}
+	else if (m <= 4.0) {
+		printf("Nao tem direito a exame\n");
+	}
+}
