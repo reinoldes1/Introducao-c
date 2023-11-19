edc22bcc9cfd3c405de79721661188c3ad95d954
reinoldes <reinoldesxbox@gmail.com>
2023-11-19 18:38:13 -0300

    Break, Continue

--- Introducao C 6/Introducao C 6.c
+++ Introducao C 6/Introducao C 6.c
@@ -4,8 +4,18 @@ int main() {
 
 	int i;
 
-	for (i = 1; i <= 50; i++) {
+	for (i = 1; i <= 15; i++) {
+		
+		if (i == 5) {
+			printf("Chegamos no '%d' ", i);
+			continue;
+		}
+
 		printf("%d ", i);
+
+		if (i == 7) {
+			break;
+		}
 	}
 
 }
